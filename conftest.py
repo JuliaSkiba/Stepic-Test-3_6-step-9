84e6d3719cbd232e542d5aa3a0a781d2467c6a84
Julia Obenlohe <skiba.y@numdes.com>
2022-07-15 16:01:25 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -7,12 +7,11 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="ru",
                      help="Choose language")
 
-
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoption("language")
+    user_language = request.config.getoption("language")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart browser for test..")
     browser = webdriver.Chrome(options=options)
     yield browser
--- test_items.py
+++ test_items.py
@@ -1,11 +1,12 @@
-import pytest
 from selenium.webdriver.common.by import By
+import time
 
 
-@pytest.fixture
-def test_items(self, browser, page):
-    link = "http://selenium1py.pythonanywhere.com/{user_language}/catalogue/coders-at-work_207/"
-    browser.get(link)
-    browser.implicitly_wait(5)
-    button = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert button is not None, "Элемент не найден"
+class Test_Button_Exists:
+    def test_button(self, browser):
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.get(link)
+        time.sleep(10)
+        browser.implicitly_wait(5)
+        button = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+        assert button is not None, "Элемент не найден"
